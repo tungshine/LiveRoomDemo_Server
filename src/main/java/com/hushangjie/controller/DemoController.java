7db3501683b5594c7f997ebacff45db93192cd5e
tungshine <362615233@qq.com>
2019-04-28 10:38:11 +0800

    update

--- src/main/java/com/hushangjie/controller/DemoController.java
+++ src/main/java/com/hushangjie/controller/DemoController.java
@@ -30,18 +30,19 @@ public class DemoController {
     private UserDao userDao;
     @Autowired
     private StatDao statDao;
-    @RequestMapping(value = "/live_room",method = RequestMethod.GET)
-    public String hello(HttpServletRequest request, Model model){
+
+    @RequestMapping(value = "/live_room", method = RequestMethod.GET)
+    public String hello(HttpServletRequest request, Model model) {
         //根据用户ip判断用户是否访问过本站
         String ip = IpUtil.getIp(request);
         Random random = new Random(20);
         HttpSession session = request.getSession();
         UserEntity user;
-        if (userDao.findOne(ip) != null){
+        if (userDao.findOne(ip) != null) {
             //用户曾经访问过
             System.out.println("用户曾经访问过");
             user = userDao.findOne(ip);
-        }else {
+        } else {
             //用户未访问过，存储用户信息
             System.out.println("用户未访问过");
             user = new UserEntity();
@@ -51,31 +52,34 @@ public class DemoController {
             userDao.save(user);
         }
         //System.out.println("ip="+ip+"name="+user.getRandomName());
-        session.setAttribute("user",user);
+        session.setAttribute("user", user);
         //判断用户是手机还是电脑端
-        if (UserAgentUtil.JudgeIsMoblie(request)){
+        if (UserAgentUtil.JudgeIsMoblie(request)) {
             //移动端访问
             return "live_m";
-        }else {
-            model.addAttribute("online_guests",getOnlineUser());
-            model.addAttribute("history_guests",getHistoryGuests());
+        } else {
+            model.addAttribute("online_guests", getOnlineUser());
+            model.addAttribute("history_guests", getHistoryGuests());
             return "live";
         }
 
     }
-    @RequestMapping(value = "/online_guests",method = RequestMethod.GET)
+
+    @RequestMapping(value = "/online_guests", method = RequestMethod.GET)
     @ResponseBody
-    public Set getOnlineUser(){
-        return  statDao.getAllUserOnline();
+    public Set getOnlineUser() {
+        return statDao.getAllUserOnline();
     }
-    @RequestMapping(value = "/history_guests",method = RequestMethod.GET)
+
+    @RequestMapping(value = "/history_guests", method = RequestMethod.GET)
     @ResponseBody
-    public List getHistoryGuests(){
+    public List getHistoryGuests() {
         return statDao.getGuestHistory();
     }
+
     @MessageMapping(value = "/chat")
     @SendTo("/topic/group")
-    public MsgEntity testWst(String message , @Header(value = "simpSessionAttributes") Map<String,Object> session){
+    public MsgEntity testWst(String message, @Header(value = "simpSessionAttributes") Map<String, Object> session) {
         UserEntity user = (UserEntity) session.get("user");
         String username = user.getRandomName();
         MsgEntity msg = new MsgEntity();
@@ -84,8 +88,9 @@ public class DemoController {
         msg.setMsgBody(message);
         return msg;
     }
-    @RequestMapping(value = "/resume",method = RequestMethod.GET)
-    public String viewMyresume(){
+
+    @RequestMapping(value = "/resume", method = RequestMethod.GET)
+    public String viewMyresume() {
         return "myresume";
     }
 
